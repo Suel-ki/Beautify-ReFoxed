158ee7715866b3940ec40dca186c4b1a10782b2d
Suel_ki <1829446054@qq.com>
2026-01-28 14:59:12 +0800

    fix: correct registerBlock call typo BLOCKS10 -> BLOCKS

--- src/main/java/io/github/suel_ki/beautify/core/init/BlockInit.java
+++ src/main/java/io/github/suel_ki/beautify/core/init/BlockInit.java
@@ -218,7 +218,7 @@ public class BlockInit {
             BlockBehaviour.Properties.ofFullCopy(Blocks.CRAFTING_TABLE));
 
     private static <T extends Block> DeferredBlock<T> register(String name, Function<BlockBehaviour.Properties, ? extends T> func, BlockBehaviour.Properties props) {
-        return BLOCKS10.registerBlock(name, func, props);
+        return BLOCKS.registerBlock(name, func, props);
     }
 
     private static DeferredBlock<LampCandelabra> registerLampCandelabra(String name) {
